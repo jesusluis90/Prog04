fbfba2e97e3107fb5843745e17b36a6a2c924e07
Jesus <jesusluiso90@gmail.com>
2016-11-17 19:22:33 +0100

    ejercicio 1 tema 4

---
+++ Ejercicio1_T4/src/ejercicio1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class ejercicio1 {
+	
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner (System.in);
+		String frase;
+		int longitudMax, contador =1;
+		
+		System.out.println("Escribe una frase (max 80 caracteres):");
+		frase=teclado.next();
+		longitudMax = frase.length();
+		
+		if (longitudMax <= 80 );
+			while (contador <= longitudMax){
+			System.out.println(frase.substring(0,contador));
+			
+			contador++;
+			}	
+
+	}
+
+}
