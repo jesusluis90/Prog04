3b2894c01410402a8c24263ca1d9818435078bde
Jesus <jesusluiso90@gmail.com>
2016-11-18 18:01:26 +0100

    ejercicio 1 tema 4

--- Ejercicio1_T4/src/ejercicio1.java
+++ Ejercicio1_T4/src/ejercicio1.java
@@ -6,17 +6,19 @@ public class ejercicio1 {
 		String frase;
 		int longitudMax, contador =1;
 		
-		System.out.println("Escribe una frase (max 80 caracteres):");
+		System.out.print("Escribe una frase (max 80 caracteres):");
 		frase=teclado.next();
 		longitudMax = frase.length();
 		
-		if (longitudMax <= 80 );
+		if (longitudMax <= 80 ){
 			while (contador <= longitudMax){
 			System.out.println(frase.substring(0,contador));
 			
 			contador++;
-			}	
-
+			}
+		}else	
+			
+			System.out.println("La frase tiene mas de 80 caracteres.");	
 	}
-
+	
 }
